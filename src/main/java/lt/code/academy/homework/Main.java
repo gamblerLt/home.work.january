7ad668df7c3f15b89bcbbf67873935cd43f949a3
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-18 22:19:06 +0200

    Komentuoti klausimai

---
+++ src/main/java/lt/code/academy/homework/Main.java
@@ -0,0 +1,39 @@
+package lt.code.academy.homework;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+/*
+* 1.egzaminas:
+* Matematiniai veiksmai. Įrašykite tik rezultatą:
+* 1. Šaknis iš 9 =
+* 2. 2 * 2 =
+* 3. 5 * 1 =
+* 4. 1 * 0 =
+* 5. 11 * 1 =
+* 6. 1 * 100 =
+* 7. 8 * 1 =
+* 8. 10 * 50 =
+* 9. 1 * 9 =
+* 10. 7 * 1 =
+*
+* --
+*
+* Nereikalauja privalomai antro, bet galima daryti sudetis
+*
+* 1. 11 + 1 =
+* 2. 1 + 100 =
+* 3. 8 + 1 =
+* 4. 10 + 50 =
+* 5. 91 + 9 =
+* 6. 7 + 8 =
+* 7. 8 + 2 =
+* 8. 7 + 7 =
+* 9. 9 + 10 =
+* 10. 10 + 10 =
+*
+*
+*
+* */
