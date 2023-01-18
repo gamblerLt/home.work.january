417bfc5b94d73b11814cbc6b7af58b3003efe9c2
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-18 22:30:12 +0200

    whatToDo

--- src/main/java/lt/code/academy/homework/WhatToDo.java
+++ src/main/java/lt/code/academy/homework/WhatToDo.java
@@ -24,6 +24,27 @@ public class WhatToDo {
     * - vaikiniuose pom (jeigu tokiu bus) dependencies versiju nereikia
     *
     * what the heck konsole
+    *
+    * -----------------------------------------------------------------
+    * Projektų techniniai reikalavimai:
+    *
+Privalomi:
+Panaudoti Collections (sąrašai, mapai ir pan.)
+Programos kodas turi būti padengtas bent 10 Unit testų
+Programos supakavimui panaudoti Maven
+Panaudoti sąsajas ir paveldėjimus, bei metodų perrašymus (override), perkrovimus (overload)
+Panaudoti Java Date Time API
+Panaudoti išimčių (Exceptions) mechanizmą
+Panaudoti Java IO API (pvz. failai)
+Panaudoti lambda ekspresijas ir srautus (streams)
+Panaudoti Enum
+Programos kodas turi būti tvarkingas, su aiškiais kintamaisiais ir lengvai skaitomas, naudojamos objekiškai orientuoto programavimo praktikos
+
+Optional:
+- Panaudoti Generics
+- programą turi būti galima paleisti naudojant komandinę eilutę
+- Panaudoti daugiagijiškumą (Multi threading)
+    *
     * */
 
 }
