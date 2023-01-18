cab3176253920ccb1edd761d4eecb9638ecf660a
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-18 22:27:04 +0200

    whatToDo

---
+++ src/main/java/lt/code/academy/homework/WhatToDo.java
@@ -0,0 +1,29 @@
+package lt.code.academy.homework;
+
+public class WhatToDo {
+
+    /*
+    *  - progrma nuskaito 2 failus:
+    *  1 - vertinimas
+    *  2 - teisingi atsakymai
+    * - rezultatai surasomi i tracia faila json formate
+    *
+    * - deserializuojant "final"gali nebuti
+    *
+    * - privalo buti dependencies
+    *
+    * - privalo buti maven
+    *  - privalo buti jason - 37 paskaita video irasymas i json
+    *
+    * - vartotojo prisijungimas
+    *
+    * - testo pasirinkimas - meniu
+    *
+    *  - validacija ????
+    *
+    * - vaikiniuose pom (jeigu tokiu bus) dependencies versiju nereikia
+    *
+    * what the heck konsole
+    * */
+
+}
